4abb4761c6a89f05dfae8aa19ee0f448906f7e6b
John Wells <molekewl2017@gmail.com>
2017-12-12 19:40:20 -0800

    css added. Burger now renders to screen. Hard coded for now.

--- building-a-burger/src/components/Layout/Layout.js
+++ building-a-burger/src/components/Layout/Layout.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import Aux from '../../hoc/Aux';
+import classes from './Layout.css'
 
 const layout = (props) => (
   // here I want adjacent elements but dont want to wrap in another div
   // high order function to wrap the elements since there is an error with return ();
   <Aux>
     <div>Toolbar, SideDrawer, Backdrop</div>
-    <main>
+    <main className={classes.Content}>
       {props.children}
     </main>
   </Aux>
