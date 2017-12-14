094c9855add6276c383dbba60e2767c762c7fcf6
John Wells <molekewl2017@gmail.com>
2017-12-14 13:01:27 -0800

    Layout, Navigation, Logo, Toolbar Header

--- building-a-burger/src/components/Layout/Layout.js
+++ building-a-burger/src/components/Layout/Layout.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css'
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout = (props) => (
   // here I want adjacent elements but dont want to wrap in another div
   // high order function to wrap the elements since there is an error with return ();
   <Aux>
-    <div>Toolbar, SideDrawer, Backdrop</div>
+    <Toolbar />
+    {/* <div>Toolbar, SideDrawer, Backdrop</div> */}
     <main className={classes.Content}>
       {props.children}
     </main>
