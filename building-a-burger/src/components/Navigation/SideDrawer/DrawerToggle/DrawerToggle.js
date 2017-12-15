974b2f9e372c1f8c96dcde5965594d0ab35eff9c
John Wells <molekewl2017@gmail.com>
2017-12-15 10:32:30 -0800

    mobile toggle feature

--- building-a-burger/src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ building-a-burger/src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
+import classes from './DrawerToggle.css'
+
 const drawerToggle = (props) => (
-  <div onClick={props.clicked}>MENU</div>
+  <div className={classes.DrawerToggle} onClick={props.clicked}>MENU</div>
 );
 
 export default drawerToggle;
