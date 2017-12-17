24168140c94564b585d8eb3af3b2a6a63aa87d0b
John Wells <molekewl2017@gmail.com>
2017-12-16 20:28:56 -0800

    more on error handling, unmount when error component isnt needed

--- building-a-burger/src/App.js
+++ building-a-burger/src/App.js
@@ -4,11 +4,25 @@ import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 
 class App extends Component {
+  // state: for testing
+  // state = {
+  //   show: true
+  // }
+
+  // // for testing
+  // componentDidMount() {
+  //   setTimeout(() => {
+  //     this.setState({show: false});
+  //   }, 5000)
+  // }
+
   render() {
     return (
       <div>
         <Layout>
           <BurgerBuilder />
+          {/* for testing on WillMount */}
+          {/* {this.state.show ? <BurgerBuilder /> : null} */}
         </Layout>
       </div>
     );
--- building-a-burger/src/hoc/withErrorHandler/withErrorHandler.js
+++ building-a-burger/src/hoc/withErrorHandler/withErrorHandler.js
@@ -12,13 +12,23 @@ const withErrorHandler = (WrappedComponent, axios) => {
     // Change to Will. because error by default when waiting on loading data from server
     // it will be called before the child components are rendered
     componentWillMount() {
-      axios.interceptors.request.use(req => {
+      this.reqInterceptor = axios.interceptors.request.use(req => {
         this.setState({error: null});
         return req;
       });
-      axios.interceptors.response.use(res => res, error => {
+      this.resInterceptor = axios.interceptors.response.use(res => res, error => {
         this.setState({error: error});
       });
+    }
+
+    // this is a lifecycle method which is executed the point of time
+    // isn't required anymore. for preventing memory leaks
+    // Also is when we don't need the burgerbuilder.js component
+    // and when the old intercepters live on with the new which we don't want.
+    componentWillUnmount() {
+      // console.log('Will Unmount', this.reqInterceptor, this.resInterceptor);
+      axios.interceptors.request.eject(this.reqInterceptor);
+      axios.interceptors.response.eject(this.resInterceptor);
     }
 
     errorConfirmedHandler = () => {
