ccac25ec34e6150518a27f744af28e3a7dfb97c6
John Wells <molekewl2017@gmail.com>
2018-01-05 04:45:16 -0800

    summary page / checkout

--- building-a-burger/src/App.js
+++ building-a-burger/src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
+import Checkout from './containers/Checkout/Checkout';
 
 class App extends Component {
   // state: for testing
@@ -20,9 +22,14 @@ class App extends Component {
     return (
       <div>
         <Layout>
-          <BurgerBuilder />
+          <Switch>
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/" component={BurgerBuilder} />
+          </Switch>
+          {/* <BurgerBuilder /> */}
           {/* for testing on WillMount */}
           {/* {this.state.show ? <BurgerBuilder /> : null} */}
+          {/* <Checkout /> */}
         </Layout>
       </div>
     );
