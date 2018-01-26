7cbf489838d9469d3a464f0493d97be53469886b
John Wells <molekewl2017@gmail.com>
2018-01-25 21:48:39 -0800

    moved state to redux checkout/order, error handling, combined reducers

--- building-a-burger/src/containers/Checkout/ContactData/ContactData.js
+++ building-a-burger/src/containers/Checkout/ContactData/ContactData.js
@@ -6,6 +6,8 @@ import Spinner from '../../../components/UI/Spinner/Spinner';
 import axios from '../../../axios-orders';
 import classes from './ContactData.css';
 import Input from '../../../components/UI/Input/Input';
+import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler';
+import * as actions from '../../../store/actions/index';
 
 class ContactData extends Component {
   state = {
@@ -91,13 +93,11 @@ class ContactData extends Component {
       },
     },
     formIsValid: false,
-    loading: false
   }
 
   orderHandler = (event) => {
     event.preventDefault();
-    console.log(this.props.ingredients)
-    this.setState({ loading: true });
+    
     const formData = {};
     for (let formElementIdentifier in this.state.orderForm) {
       formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
@@ -108,15 +108,8 @@ class ContactData extends Component {
       price: this.props.price,
       orderData: formData,
     }
-    axios.post('/orders.json', order)
-      .then(response => {
-        // we want to stop the request whether the was an error or not
-        this.setState({ loading: false, purchasing: false });
-        this.props.history.push('/');
-      })
-      .catch(error => {
-        this.setState({ loading: false, purchasing: false });
-      });
+
+    this.props.onOrderBurger(order);
   }
 
   checkValidity(value, rules) {
@@ -184,7 +177,7 @@ class ContactData extends Component {
         <Button btnType="Success" disabled={!this.state.formIsValid}>ORDER</Button>
       </form>
     );
-    if (this.state.loading) {
+    if (this.props.loading) {
       form = <Spinner />;
     }
 
@@ -199,9 +192,16 @@ class ContactData extends Component {
 
 const mapStateToProps = state => {
   return {
-    ings: state.ingredients,
-    price: state.totalPrice
+    ings: state.burgerBuilder.ingredients,
+    price: state.burgerBuilder.totalPrice,
+    loading: state.order.loading
   }
 }
 
-export default connect(mapStateToProps)(ContactData);
+const mapDispatchToProps = dispatch => {
+  return {
+    onOrderBurger: (orderData) => dispatch(actions.purchaseBurger(orderData))
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(ContactData, axios));
