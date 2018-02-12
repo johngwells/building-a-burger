d9c7e7b84742c15eaf104c03c25f57ef08ba6955
John Wells <molekewl2017@gmail.com>
2018-02-12 14:18:27 -0800

    bug: after you add an ingredient adding or removing cause an error.

--- building-a-burger/src/store/reducers/burgerBuilder.js
+++ building-a-burger/src/store/reducers/burgerBuilder.js
@@ -27,13 +27,14 @@ const addIngredient = (state, action) => {
 }
 
 const removeIngredient = (state, action) => {
-  return {
-    ...state,
-    ingredients: {
-      [action.ingredientName]: state.ingredients[action.ingredientName] - 1,
-      building: true
-    }
-  };
+  const updatedIngredient = { [action.ingredientName]: state.ingredients[action.ingredientName] - 1 }
+  const updatedIngredients = updateObject(state.ingredients, updatedIngredient);
+  const updatedState = {
+    ingredients: updatedIngredients,
+    totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName],
+    building: true
+  }
+  return updateObject(state, updatedState);
 }
 
 const setIngredients = (state, action) => {
